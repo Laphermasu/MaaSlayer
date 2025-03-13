c59d4fb244a3a2653f3271120a7fa2fc2a39f99d
Environ123 <98010328+Environ123@users.noreply.github.com>
2025-03-13 18:11:22 +0800

    PLAY
    PLAY

--- test_main_lyh.py
+++ test_main_lyh.py
@@ -3,79 +3,93 @@ from maa.toolkit import Toolkit
 from maa.context import Context
 from maa.resource import Resource
 from maa.controller import AdbController
-from maa.library import Library
 from maa.custom_action import CustomAction
-from src.core.data_models import Monster, Cards
+from src.core.data_models import Monster, Cards ,Event
 from src.core.data_models import Player
 from src.custom_recognition.monster_recognition import MonsterRecognition
 from src.custom_recognition.player_recognition import PlayerRecognition
 from src.custom_recognition.event_recognition import EventRecognition
 from src.custom_recognition.cards_recogntion import CardRecognition
 from src.utils.json_utils import JsonUtils
-# from sb3_contrib.ppo_mask import MaskablePPO
-# from src.custom_action.adb_action import ADBAction
-# from SlayTheSpireRL.slay_the_spire_env import SlayTheSpireEnv
+from sb3_contrib.ppo_mask import MaskablePPO
+from SlayTheSpireRL.slay_the_spire_env import SlayTheSpireEnv
 import json
 import threading
-# import torch as th
-# from func import (
-#     get_available_commands,
-#     get_screen,
-#     get_hand,
-#     get_monsters,
-#     get_player,
-#     get_deck,
-#     get_relics,
-#     get_potions,
-#     get_map,
-#     get_info
-# )
+import torch as th
+from func import (
+    get_available_commands,
+    get_deck,
+    get_relics,
+    get_map
+)
 
 resource = Resource()
 
-def generate_json(monsters, cards):
-    available_commands = get_available_commands()
-    screen = get_screen()
-    combat_state = {
-        "monsters": [monster.__dict__ for monster in monsters],
-        # "monsters": get_monsters(),
-        "hand": [card.__dict__ for card in cards],
-        # "hand": get_hand(),
-        # "player": [player.__dict__ for player in players],
-        "player": get_player(),
-    }
+
+def generate_json(screen_type ,monsters = None,events = None,cards= None,player=None):
     deck = get_deck()
     relics = get_relics()
-    max_hp, gold, current_hp, floor, room_type = get_info()
-    potions = get_potions()
     game_map = get_map()
+    if screen_type == "NONE":
+        available_commands = get_available_commands()
+        combat_state = {
+            "monsters": [monster.__dict__ for monster in monsters],
+            "hand": [card.__dict__ for card in cards],
+            "player": {
+                "block": player.block,
+                "energy": player.energy,
+                "powers": player.powers
+            }
+        }
+        game_state = {
+            "screen_type": screen_type,
+            "screen_state": {},
+            "combat_state": combat_state,
+            "deck": deck,
+            "relics": relics,
+            "max_hp": player.max_hp,
+            "gold": player.gold,
+            "potions": [],
+            "current_hp": player.current_hp,
+            "floor": player.floor,
+            "map": game_map,
+            "ascension_level": 0,
+        }
 
-    game_state = {
-        "screen_type": screen['type'],
-        "screen_state": screen['state'],
-        "combat_state": combat_state,
-        "deck": deck,
-        "relics": relics,
-        "max_hp": max_hp,
-        "gold": gold,
-        "potions": potions,
-        "current_hp": current_hp,
-        "floor": floor,
-        "map": game_map,
-        "room_type": room_type,
-    }
+        json_data = {
+            "available_commands": available_commands,
+            "ready_for_command": True,
+            "in_game": True,
+            "game_state": game_state
+        }
+        json_result = json.dumps(json_data)
 
-    json_data = {
-        "available_commands": available_commands,
-        "ready_for_command": True,
-        "in_game": True,
-        "game_state": game_state
-    }
+    elif screen_type == "EVENT":
+        game_state = {
+            "screen_type": screen_type,
+            "screen_state": {
+                "event_id": events.event_id,
+                "options": events.options,
+                "deck": deck,
+                "relics": relics,
+                "max_hp": player.max_hp,
+                "gold": player.gold,
+                "potions": [],
+                "current_hp": player.current_hp,
+                "floor": player.floor,
+                "map": game_map,
+                "ascension_level": 0,
+            }
+        }
 
+        json_data = {
+            "game_state": game_state
+        }
 
-    json_result = json.dumps(json_data)
+        json_result = json.dumps(json_data)
 
     return json_result
+
 
 def recognize_monsters(tasker, result_dict):
     print("开始识别怪物")
@@ -84,10 +98,11 @@ def recognize_monsters(tasker, result_dict):
     }
     task_detail = tasker.post_task("MyRecongitionEntry", pipeline_override).wait().get()
     monsters = JsonUtils.deserialize_from_str(
-        JsonUtils.serialize_to_str(task_detail.nodes[0].recognition.best_result.detail),Monster
+        JsonUtils.serialize_to_str(task_detail.nodes[0].recognition.best_result.detail), Monster
     )
     result_dict["monsters"] = monsters  # 存储到共享字典
     print(f"识别到的怪物: {monsters}")
+
 
 def recognize_players(tasker, result_dict):
     print("开始识别玩家")
@@ -96,10 +111,11 @@ def recognize_players(tasker, result_dict):
     }
     task_detail = tasker.post_task("MyRecongitionEntry", pipeline_override).wait().get()
     players = JsonUtils.deserialize_from_str(
-        JsonUtils.serialize_to_str(task_detail.nodes[0].recognition.best_result.detail)
+        JsonUtils.serialize_to_str(task_detail.nodes[0].recognition.best_result.detail), Player
     )
     result_dict["players"] = players
     print(f"识别到的玩家: {players}")
+
 
 def recognize_events(tasker, result_dict):
     print("开始识别事件")
@@ -108,10 +124,11 @@ def recognize_events(tasker, result_dict):
     }
     task_detail = tasker.post_task("MyRecongitionEntry", pipeline_override).wait().get()
     events = JsonUtils.deserialize_from_str(
-        JsonUtils.serialize_to_str(task_detail.nodes[0].recognition.best_result.detail)
+        JsonUtils.serialize_to_str(task_detail.nodes[0].recognition.best_result.detail), Event
     )
     result_dict["events"] = events
     print(f"识别到的事件: {events}")
+
 
 def recognize_cards(tasker, result_dict):
     print("开始识别卡牌")
@@ -120,10 +137,11 @@ def recognize_cards(tasker, result_dict):
     }
     task_detail = tasker.post_task("MyRecongitionEntry", pipeline_override).wait().get()
     cards = JsonUtils.deserialize_from_str(
-        JsonUtils.serialize_to_str(task_detail.nodes[0].recognition.best_result.detail),Cards
+        JsonUtils.serialize_to_str(task_detail.nodes[0].recognition.best_result.detail), Cards
     )
     result_dict["cards"] = cards
     print(f"识别到的卡牌: {cards}")
+
 
 def main():
     # 初始化 MaaFramework
@@ -147,55 +165,57 @@ def main():
         adb_path=device.adb_path,
         address=device.address,
         screencap_methods=device.screencap_methods,
-        input_methods= 1,
+        input_methods=device.input_methods,
         config=device.config,
     )
     controller.post_connection().wait()
     print("设备连接成功")
+
     print("初始化tasker")
     tasker = Tasker()
     # tasker = Tasker(notification_handler=MyNotificationHandler())
     tasker.bind(resource, controller)
-    # adb_action = ADBAction(controller, Context(resource))
+
     if not tasker.inited:
         print("Failed to init MAA.")
         exit()
     print("tasker初始化完成")
 
-    # 注册自定义识别器
     resource.register_custom_recognition("monsterRecognition", MonsterRecognition())
-    # resource.register_custom_recognition("playerRecognition", PlayerRecognition())
-    # resource.register_custom_recognition("eventRecognition", EventRecognition())
+    resource.register_custom_recognition("playerRecognition", PlayerRecognition())
+    resource.register_custom_recognition("eventRecognition", EventRecognition())
     resource.register_custom_recognition("cardRecognition", CardRecognition())
     resource.register_custom_action("ADBAction", ADBAction())
 
+
     # 共享字典存储识别结果
-    # result_dict = {}
-    #
-    # # 创建线程并执行
-    # threads = [
-    #     threading.Thread(target=recognize_monsters, args=(tasker, result_dict)),
-    #     threading.Thread(target=recognize_players, args=(tasker, result_dict)),
-    #     threading.Thread(target=recognize_events, args=(tasker, result_dict)),
-    #     threading.Thread(target=recognize_cards, args=(tasker, result_dict))
-    # ]
-    #
-    # for thread in threads:
-    #     thread.start()
-    #
-    # for thread in threads:
-    #     thread.join()
-    #
-    # print("所有识别任务完成")
-    #
-    # # 解析 JSON
-    # monsters = result_dict.get("monsters", [])
-    # players = result_dict.get("players", [])
-    # events = result_dict.get("events", [])
-    # cards = result_dict.get("cards", [])
-    #
-    # game_state = json.loads(generate_json(monsters,cards))
-    # print(game_state)
+    result_dict = {}
+
+    # 创建线程并执行
+    threads = [
+        threading.Thread(target=recognize_monsters, args=(tasker, result_dict)),
+        threading.Thread(target=recognize_players, args=(tasker, result_dict)),
+        threading.Thread(target=recognize_events, args=(tasker, result_dict)),
+        threading.Thread(target=recognize_cards, args=(tasker, result_dict))
+    ]
+
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+    print("所有识别任务完成")
+
+    # 解析 JSON
+    monsters = result_dict.get("monsters", [])
+    # print(monsters)
+    players = result_dict.get("players", [])
+    events = result_dict.get("events", [])
+    cards = result_dict.get("cards", [])
+    game_state = json.loads(generate_json(screen_type="EVENT",monsters=monsters,events=events,cards=cards,player=players))
+
+
     # env = SlayTheSpireEnv({})
     # device = th.device("cuda" if th.cuda.is_available() else "cpu")
     # model = MaskablePPO.load("maskable_ppo_slay_the_spire1.zip", env=env, device=device)
@@ -214,12 +234,11 @@ def main():
     # action = int(action)
     # chosen_command = env.actions[action]
     # print(f"Action: {chosen_command}")
-    # adb_action.execute(chosen_command, cards, monsters)
+
     print("pipeline定义")
     pipeline_override = {
-        "ADBAction": {"action": "custom", "custom_action": "ADBAction"},
+        "ADBAction": {"action": "custom", "custom_action": "ADBAction", "custom_action_param": game_state},
     }
-    # "ADBAction": {"action": "custom", "custom_action": "ADBAction", "custom_action_param": game_state},
     print("pipeline选中任务执行")
     tasker.post_task("ADBAction", pipeline_override).wait().get()
     print("任务执行完成")
@@ -241,8 +260,17 @@ class ADBAction(CustomAction):
         :return: RunResult(success=True/False)
         """
         print("开始执行 ADBAction 自定义动作")
-        command = "START 1234"
-        # command = argv.args[0] if argv.args else None
+        command = "PLAY 1"
+        game_state = argv.custom_action_param
+
+        game_state = json.loads(game_state)
+        game_state = game_state["game_state"]
+        print(game_state)
+        combat_state = game_state.get("combat_state", {})
+        monsters = combat_state.get("monsters", [])
+
+        cards = combat_state.get("hand", [])
+        # print(cards)
         parts = command.split()
         action_type = parts[0]
 
@@ -251,36 +279,14 @@ class ADBAction(CustomAction):
             print("开始游戏")
             img = context.tasker.controller.post_screencap().wait().get()
             # 下面这个run_task是能够成功点击到Play的
-            context.run_task("StartGame",pipeline_override={
-                    "Click": {
-                        "action": "Click",
-                        "target": [72,423,86,49]
-                    },
-                    "StartGame": {
-                        "recognition": "OCR",
-                        "expected": "Play",
-                        "action": "Click"
-                }})
-            # 下面的只做了识别，没有点击到Play
-            reco_detail = context.run_recognition(
-                "StartGame",  # 流水线名称
-                img,  # 输入图像
-                pipeline_override={
-                    "StartGame": {
-                        "recognition": "OCR",
-                        "expected": "Play",
-                        "action": "Click"
-                }}
-            )
-            # 下面的只做了点击[0,0,0,0]，没有识别到Play
-            detail = context.run_action(
+            context.run_task(
                 "StartGame",
                 pipeline_override={
                     "StartGame": {
                         "recognition": "OCR",
                         "expected": "Play",
-                        "action": "Click"
-                        # "next": "standard"/
+                        "action": "Click",
+                        "next": "standard"
                     },
                     "standard": {
                         "recognition": "OCR",
@@ -290,58 +296,62 @@ class ADBAction(CustomAction):
                     },
                     "character": {
                         "recognition": "TemplateMatch",
-                        "template": "assets\\resource\\image\\铁甲战士.png",
+                        "template": "铁甲战士.png",
                         "action": "Click",
                         "next": "embark"
                     },
                     "embark": {
                         "recognition": "OCR",
                         "expected": "Embark",
+                        "roi": [1000,500,250,250],
                         "action": "Click",
                     }
                 }
             )
+
         elif action_type == "PLAY":
             card_index = int(parts[1]) - 1
-            print(f"打出卡牌：索引 {card_index}")
-
+            # print(f"打出卡牌：索引 {card_index}")
             card_info = cards[card_index]
-            card_box = card_info.get("box", None)
-
+            card_name = card_info.get("name", None)
+            converted_monster_box = []
+            print(card_name)
             if len(parts) > 2:
                 target_index = int(parts[2])
-                if target_index < 0 or target_index >= len(monsters):
-                    print(f"错误: 目标索引 {target_index} 超出范围")
-                    return
-                monster_info = monsters[target_index]
+                monster_info = monsters[target_index - 1]
                 monster_box = monster_info.get("box", None)
-            else:
-                monster_box = [100, 575, 40, 40]  # 默认目标
-
-                context.run_action(
-                "selectcard",
+                converted_monster_box = [monster_box["x"], monster_box["y"], monster_box["w"], monster_box["h"]]
+                # print(converted_monster_box)
+            elif len(parts) < 3:
+                converted_monster_box = [643,414, 40, 40]  # 默认目标
+                # converted_monster_box = [100, 575, 40, 40]  # 默认目
+            # img = context.tasker.controller.post_screencap().wait().get()
+            context.run_task(
+                "Click1",
                 pipeline_override={
-                    "selectcard": {
-                        "action": "click",
-                        "target": card_box,
-                        "next": "selectmonster"
+                    "Click1": {
+                        "recognition": "OCR",
+                        "expected": card_name,
+                        "action": "Click",
+                        "next": "Click2"
                     },
-                    "selectmonster": {
-                        "action": "click",
-                        "target": monster_box,
-                    },
+                    "Click2": {
+                        "action": "Click",
+                        "target": converted_monster_box
+                    }
                 }
-                )
+            )
+            print("123")
 
         elif action_type == "END":
             """结束回合"""
             print("结束回合")
-            context.run_action(
+            context.run_task(
                 "end_turn",
                 pipeline_override={
                     "end_turn": {
                         "recognition": "OCR",
-                        "expected": "结束回合",
+                        "expected": "End Turn",
                         "action": "Click"
                     }
                 }
@@ -349,12 +359,12 @@ class ADBAction(CustomAction):
 
         elif action_type == "PROCEED":
             print("前进")
-            context.run_action(
+            context.run_task(
                 "proceed",
                 pipeline_override={
                     "proceed": {
                         "recognition": "OCR",
-                        "expected": "前进",
+                        "expected": "PROCEED",
                         "action": "Click"
                     }
                 }
@@ -363,7 +373,7 @@ class ADBAction(CustomAction):
         elif action_type == "RETURN":
             """返回上一级菜单"""
             print("返回")
-            context.run_action(
+            context.run_task(
                 "return_to_previous",
                 pipeline_override={
                     "return_to_previous": {
@@ -375,7 +385,7 @@ class ADBAction(CustomAction):
             )
 
         elif action_type == "CONFIRM":
-            context.run_action(
+            context.run_task(
                 "confirm",
                 pipeline_override={
                     "confirm": {
@@ -402,24 +412,24 @@ class ADBAction(CustomAction):
 
         elif action_type == "CHOOSE":
             """选择事件中的某个选项"""
-            context.run_action(
+            context.run_task(
                 "choose_option",
                 pipeline_override={
                     "choose_option": {
                         "recognition": "TemplateMatch",
-                        "template": "assets\\resource\\image\\option.png",
+                        "template": "option.png",
                         "index": int(parts[1]),
                         "action": "Click"
                     }
                 }
             )
-
         else:
             print(f"未知的命令: {command}")
         # **调用 execute() 来处理**
 
 
         return True
+
 
 
 
